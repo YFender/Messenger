317b4f211412241d9e2043e7ac473382f7e43887
YFen <yurik2159@inbox.ru>
2022-02-22 16:28:22 +0300

    h

--- client/main.py
+++ client/main.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from client import *
 from login import *
+from registration import *
 
 class MyWin(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -9,14 +10,27 @@ class MyWin(QtWidgets.QMainWindow):
         self.ui = Ui_Client_MainWindow()
         self.ui.setupUi(self)
 
+        self.ui.login_pushbutton.clicked.connect(self.login)
+        self.ui.reg_pushbutton.clicked.connect(self.registration)
+
     def login(self):
         self.w2 = Login()
         self.w2.show()
 
-class Login(QtWidgets.QWidget):
+    def registration(self):
+        self.w3 = Registration()
+        self.w3.show()
+
+class Login(QtWidgets.QMainWindow):
     def __init__(self):
         super(Login, self).__init__()
         self.ui = Ui_Login()
+        self.ui.setupUi(self)
+
+class Registration(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Registration, self).__init__()
+        self.ui = Ui_Registration()
         self.ui.setupUi(self)
 
 if __name__ == "__main__":
